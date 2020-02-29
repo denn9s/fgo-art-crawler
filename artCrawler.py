75d0fd19a0cfc12bcf5b5596781f900aa7aed26b
denn9s <cs.dennisong@gmail.com>
2020-02-29 02:19:05 -0800

    Retrieves HTML content of main page with list of servants and saves to servantListPage.html

--- artCrawler.py
+++ artCrawler.py
@@ -2,8 +2,16 @@ import urllib.request
 import urllib.error
 import urllib.parse
 
+BASE_URL = 'https://grandorder.wiki/Servant_List'
+
 def main():
-	pass
+	request = urllib.request.Request(BASE_URL, headers = {'User-Agent': 'Mozilla/5.0'})
+	response = urllib.request.urlopen(request)
+	content = response.read()
+
+	file = open('servantListPage.html', 'wb')
+	file.write(content)
+	file.close
 
 if __name__ == '__main__':
 	main()
